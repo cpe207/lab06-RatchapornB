4fd01a7368e01a36c9e236dcd8c79151d4416a97
RatchapornB <ratchaporn_b@cmu.ac.th>
2024-07-08 22:49:19 +0700

    Completed

--- q1.ts
+++ q1.ts
@@ -4,11 +4,14 @@ import axios from "axios";
 /* assign interface/type to the function definition properly */
 const getUser = async (userId: number) => {
   try {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users/${userId}");
-    const username = res.data.name
-    return username;
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users/");
+    const finduser = res.data.find((user : any) => user.id === userId);
+    
+    return finduser.name; 
   }
+
   catch (error) {
+
     return "INVALID USER ID";
   }
  
